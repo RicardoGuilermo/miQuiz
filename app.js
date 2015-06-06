0454b472765b305481a98cd11e2fd682d3cc6ec3
RicardoGuilermo <ptasentamientos@gmail.com>
2015-06-06 13:28:11 -0300

    Primera pregunta
    Signed-off-by: RicardoGuilermo <ptasentamientos@gmail.com>
    Corresponde a la 1 pregunta con una solo question = Roma

--- app.js
+++ app.js
@@ -6,9 +6,16 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
-//var users = require('./routes/users');
+
 
 var app = express();
+
+
+
+
+
+
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -24,7 +31,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-//app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
